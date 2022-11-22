057f1b776bd3ac6b436e65f1741d52dfe142d693
WoojinDeve <jwj060111@gmail.com>
2022-11-22 13:12:21 +0900

    feat: 현재 일로 날짜 수정

--- src/main/java/domain/MovieRepository.java
+++ src/main/java/domain/MovieRepository.java
@@ -1,7 +1,4 @@
-package domain.repository;
-
-import domain.entity.Movie;
-import domain.entity.PlaySchedule;
+package domain;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,26 +10,26 @@ public class MovieRepository {
 
     static {
         Movie movie1 = new Movie(1, "생일", 8_000);
-        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 12:00"), 6));
-        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 14:40"), 6));
-        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 17:00"), 6));
-        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 19:40"), 3));
-        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 22:00"), 3));
+        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 12:00"), 6));
+        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 14:40"), 6));
+        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 17:00"), 6));
+        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 19:40"), 3));
+        movie1.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 22:00"), 3));
         movies.add(movie1);
 
         Movie movie2 = new Movie(5, "돈", 10_000);
-        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 08:00"), 3));
-        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 10:30"), 5));
-        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 13:00"), 5));
-        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 15:30"), 5));
+        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 08:00"), 3));
+        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 10:30"), 5));
+        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 13:00"), 5));
+        movie2.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 15:30"), 5));
         movies.add(movie2);
 
         Movie movie3 = new Movie(7, "파이브피트", 9_000);
-        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 13:00"), 4));
-        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 15:40"), 4));
-        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 18:00"), 4));
-        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 20:40"), 3));
-        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2019-04-16 23:15"), 3));
+        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 13:00"), 4));
+        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 15:40"), 4));
+        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 18:00"), 4));
+        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 20:40"), 3));
+        movie3.addPlaySchedule(new PlaySchedule(createDateTime("2022-11-22 23:15"), 3));
         movies.add(movie3);
 
         Movie movie4 = new Movie(8, "덤보", 9_000);
