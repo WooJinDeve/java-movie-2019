acba774c2795f552ac2bc93928ecf020ed664376
WoojinDeve <jwj060111@gmail.com>
2022-11-22 15:28:52 +0900

    feat: 결제 정보 및 예약을 확인하는 ENUM 추가

---
+++ src/main/java/domain/PayGroupAdvanced.java
@@ -0,0 +1,31 @@
+package domain;
+
+import java.util.Arrays;
+
+public enum PayGroupAdvanced {
+    CASH(1,"현금",0.98),
+    CARD(2,"카드",0.95);
+    private int typeNum;
+    private String title;
+    private double discountRete;
+    PayGroupAdvanced(int typeNum, String title, double discountRete) {
+        this.typeNum = typeNum;
+        this.title = title;
+        this.discountRete = discountRete;
+    }
+
+    public static PayGroupAdvanced findByPayType(int typeNum){
+        return Arrays.stream(PayGroupAdvanced.values())
+                .filter(payGroup -> payGroup.hasTypeNum(typeNum))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private boolean hasTypeNum(int typeNum){
+        return this.typeNum == typeNum;
+    }
+
+    public static double discountPrice(PayGroupAdvanced payGroupAdvanced, int price){
+        return price * payGroupAdvanced.discountRete;
+    }
+}
