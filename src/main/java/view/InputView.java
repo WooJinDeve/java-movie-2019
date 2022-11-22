7c972f851551a4256032f83a21978b5bed4faff2
WoojinDeve <jwj060111@gmail.com>
2022-11-22 10:54:29 +0900

    feat: 정수가 아닐경우 예외발생 코드 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,12 +1,18 @@
 package view;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
+    private static final String INPUT_MOVIE_TEXT = "## 예약할 영화를 선택하세요.";
 
     public static int inputMovieId() {
-        System.out.println("## 예약할 영화를 선택하세요.");
-        return scanner.nextInt();
+        System.out.println(INPUT_MOVIE_TEXT);
+        try {
+            return scanner.nextInt();
+        }catch (InputMismatchException exception){
+            throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다.");
+        }
     }
 }
