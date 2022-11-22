3fea6c215258da5fca020060254d24feaeac383f
WoojinDeve <jwj060111@gmail.com>
2022-11-22 13:07:47 +0900

    feat : 영화 예매 기능 입력 및 출력

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,13 +6,35 @@ import java.util.Scanner;
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String INPUT_MOVIE_TEXT = "## 예약할 영화를 선택하세요.";
+    private static final String INPUT_SCHEDULE_TEXT = "## 예약할 시간를 선택하세요.";
+    private static final String INPUT_CAPACITY_TEXT = "## 예인 인원을 입력하세요.";
+    private static final String INPUT_RESERVE_TEXT = "## 예약 확인 입니다. [ 1: 예약, 2: 추가 예약 ]";
 
     public static int inputMovieId() {
         System.out.println(INPUT_MOVIE_TEXT);
+        return getInputInt();
+    }
+
+    public static int inputSchedule(){
+        System.out.println(INPUT_SCHEDULE_TEXT);
+        return getInputInt();
+    }
+
+    public static int inputCapacity(){
+        System.out.println(INPUT_CAPACITY_TEXT);
+        return getInputInt();
+    }
+
+    public static int intCheckReserve(){
+        System.out.println(INPUT_RESERVE_TEXT);
+        return getInputInt();
+    }
+
+    private static int getInputInt(){
         try {
             return scanner.nextInt();
         }catch (InputMismatchException exception){
-            throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다.");
+            return getInputInt();
         }
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,13 +1,29 @@
 package view;
 
+import domain.BookedMovie;
 import domain.Movie;
+import domain.PlaySchedule;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class OutputView {
     public static void printMovies(List<Movie> movies) {
         for (Movie movie : movies) {
-            System.out.println(movie);
+            movie.movieInformationOutput();
+        }
+    }
+
+    public static void printSchedule(Movie movie){
+        movie.movieInformationOutput();
+        for (PlaySchedule playSchedule : movie.getPlaySchedules()) {
+            playSchedule.scheduleInformationOutput();
+        }
+    }
+
+    public static void printBookedMovies(List<BookedMovie> bookedMovies){
+        for (BookedMovie bookedMovie : bookedMovies) {
+            bookedMovie.bookedMovieInformationOutput();
         }
     }
 }
